a44732f2fc1bc71ff0d647c7b23f337c57198d8d
hakantrg2 <85925108+hakantrg2@users.noreply.github.com>
2021-10-12 23:24:25 +0300

    log

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
+	username := os.Getenv("MONGODB_USERNAME")
+	password := os.Getenv("MONGODB_PASSWORD")
+	clusterEndpoint := os.Getenv("MONGODB_ENDPOINT")
+	fmt.Print(username)
+	fmt.Print(password)
+	fmt.Print(clusterEndpoint)
+
 	r := gin.Default()
 	r.GET("/ping", handlePing)
 	r.GET("/user/:name/:deviceId", handleGetUserInfo)
